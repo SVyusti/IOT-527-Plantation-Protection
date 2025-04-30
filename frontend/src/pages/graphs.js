535f7e6bd7f2bda8edfebf8341d9ab6fd0582199
nishita2104 <nishita2104@gmail.com>
2025-05-01 01:40:17 +0530

    graphs

--- frontend/src/pages/graphs.js
+++ frontend/src/pages/graphs.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Graphs = () => {
-  return <div>Graphs Page</div>;
+const Graph = () => {
+  return <iframe
+  src="http://13.232.179.26:3000/d/eekhx3o5btbswf/iot-data?orgId=1&from=now-6h&to=now&timezone=browser"
+  width="100%"
+  height="1200"
+  frameBorder="0"
+  title="Grafana Dashboard"
+/>
 };
 
-export default Graphs;
+export default Graph;
